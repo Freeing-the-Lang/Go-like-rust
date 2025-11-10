48ea44043ecefdd6562c09f25c7f07195db41c1b
한진희 <0200134hjh@gmail.com>
2025-11-10 11:49:34 +0900

    Create heap.go

---
+++ runtime/heap.go
@@ -0,0 +1,27 @@
+package runtime
+
+import (
+	"fmt"
+	"sync"
+)
+
+type HeapObject struct {
+	Type string
+	Data any
+}
+
+var heap = sync.Pool{
+	New: func() any { return &HeapObject{} },
+}
+
+func Alloc(t string, v any) *HeapObject {
+	obj := heap.Get().(*HeapObject)
+	obj.Type = t
+	obj.Data = v
+	return obj
+}
+
+func Release(o *HeapObject) {
+	// Go GC가 자동 처리하므로 명시 해제 불필요
+	heap.Put(o)
+}
